a3fdd29efc980aa35c41b2f1e0f77463f382d1e5
Jose Soto <jsoto7087@gmail.com>
2023-02-18 11:56:05 -0500

    CSS update

--- src/components/styles/themes.js
+++ src/components/styles/themes.js
@@ -4,3 +4,4 @@ export const styleBodyTheme = {
   menu: "width: fit-content; font-size: small; font-weight: 500; font-family: Georgia, serif;", 
   body: " float: right; width: 20%; padding: 25px; font-weight: bold; font-family: times;"
 }
+// thsi

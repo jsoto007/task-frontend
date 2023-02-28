1c5295d538e9b5268641e8c278078c82e15e6814
Jose Soto <jsoto7087@gmail.com>
2023-02-27 23:37:53 -0500

    update

--- src/components/styles/themes.js
+++ src/components/styles/themes.js
@@ -2,5 +2,5 @@
 
 export const styleBodyTheme = {
   menu: "width: fit-content; font-size: small; font-weight: 500; font-family: Georgia, serif; ", 
-  body: " float: right; width: 20%; padding: 2px; font-weight: bold; font-family: times; border-radius: 38px; margin: 20px; background-color: #8eb5d36b;"
+  body: " float: right; width: 20%; padding: 2px; font-weight: bold; font-family: times; border-radius: 38px; margin: 20px; background-color: #d1d1d15e;"
 }
